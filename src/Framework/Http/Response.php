0a572ebc95fbfa223c50e453559b18f2afafa519
Sergey Nazarov <app06mt@gmail.com>
2019-10-23 14:43:57 +0300

    psr request/response

--- src/Framework/Http/Response.php
+++ src/Framework/Http/Response.php
@@ -1,54 +1,47 @@
 <?php
-
 namespace Framework\Http;
-
-class Response
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+class Response implements ResponseInterface
 {
     private $headers = [];
     private $body;
     private $statusCode;
     private $reasonPhrase = '';
-
     private static $phrases = [
-      200 => 'OK',
-      301 => 'Moved Permanently',
-      400 => 'Bad Request',
-      403 => 'Forbidden',
-      404 => 'Not Found',
-      500 => 'Internal Server Error'
+        200 => 'OK',
+        301 => 'Moved Permanently',
+        400 => 'Bad Request',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        500 => 'Internal Server Error',
     ];
-
     public function __construct($body, $status = 200)
     {
-        $this->body = $body;
+        $this->body = $body instanceof StreamInterface ? $body : new Stream($body);
         $this->statusCode = $status;
     }
-
-    public function getBody()
+    public function getBody(): StreamInterface
     {
         return $this->body;
     }
-
-    public function withBody($body): self
+    public function withBody(StreamInterface $body): self
     {
         $new = clone $this;
         $new->body = $body;
         return $new;
     }
-
     public function getStatusCode(): int
     {
         return $this->statusCode;
     }
-
     public function getReasonPhrase()
     {
-        if(!$this->reasonPhrase && isset(self::$phrases[$this->statusCode])) {
+        if (!$this->reasonPhrase && isset(self::$phrases[$this->statusCode])) {
             $this->reasonPhrase = self::$phrases[$this->statusCode];
         }
         return $this->reasonPhrase;
     }
-
     public function withStatus($code, $reasonPhrase = ''): self
     {
         $new = clone $this;
@@ -56,32 +49,45 @@ class Response
         $new->reasonPhrase = $reasonPhrase;
         return $new;
     }
-
-    public function getHeaders()
+    public function getHeaders(): array
     {
         return $this->headers;
     }
-
-    public function hasHeader($header): bool
+    public function hasHeader($name): bool
     {
-        return isset($this->headers[$header]);
+        return isset($this->headers[$name]);
     }
-
-    public function getHeader($header): bool
+    public function getHeader($name)
     {
-        if(!$this->hasHeader($header)) {
+        if (!$this->hasHeader($name)) {
             return null;
         }
-        return $this->headers[$header];
+        return $this->headers[$name];
     }
-
-    public function withHeader($header, $value): self
+    public function withHeader($name, $value): self
     {
         $new = clone $this;
-        if ($new->hasHeader($header)) {
-            unset($new->headers[$header]);
+        if ($new->hasHeader($name)) {
+            unset($new->headers[$name]);
         }
-        $new->headers[$header] = $value;
+        $new->headers[$name] = (array)$value;
         return $new;
     }
+    public function withAddedHeader($name, $value): self
+    {
+        $new = clone $this;
+        $new->headers[$name] = array_merge($new->headers[$name], (array)$value);
+        return $new;
+    }
+    public function withoutHeader($name): self
+    {
+        $new = clone $this;
+        if ($new->hasHeader($name)) {
+            unset($new->headers[$name]);
+        }
+        return $new;
+    }
+    public function getProtocolVersion() {}
+    public function withProtocolVersion($version) {}
+    public function getHeaderLine($name) {}
 }
