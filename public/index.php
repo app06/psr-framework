637f921160f0f04b9b257eb67f200c88b81dbe49
Sergey Nazarov <app06mt@gmail.com>
2019-10-23 14:17:44 +0300

    Request, Response

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+use Framework\Http\RequestFactory;
+use Framework\Http\Response;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+$request = RequestFactory::fromGlobals();
+
+$name = $request->getQueryParams()['name'] ?? 'Guest';
+
+$response = (new Response('Hello,' . $name . '!'))
+    ->withHeader('X-Developer', 'Sergey');
+
+header('HTTP/1.0 ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
+
+foreach ($response->getHeaders() as $name => $value) {
+    header($name . ':' . $value);
+}
+
+echo $response->getBody();
