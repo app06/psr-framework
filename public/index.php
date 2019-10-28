c39ab17e22480081926b04ba7c969f44eae562a9
Sergey Nazarov <app06mt@gmail.com>
2019-10-28 21:02:27 +0300

    add simple actions

--- public/index.php
+++ public/index.php
@@ -2,17 +2,54 @@
 
 use Zend\Diactoros\ServerRequestFactory;
 use Zend\Diactoros\Response\HtmlResponse;
-use Framework\Http\ResponseSender;
+use Zend\Diactoros\Response\JsonResponse;
+use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
+use Psr\Http\Message\ServerRequestInterface;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
+### Initialization
 $request = ServerRequestFactory::fromGlobals();
 
-$name = $request->getQueryParams()['name'] ?? 'Guest';
+### Action
+$path = $request->getUri()->getPath();
+$action = null;
 
-$response = (new HtmlResponse('Hello,' . $name . '!'))
-    ->withHeader('X-Developer', 'Sergey');
+if ($path === '/') {
+    $action = function (ServerRequestInterface $request) {
+        $name = $request->getQueryParams()['name'] ?? 'Guest';
+        return new HtmlResponse('Hello, ' . $name . '!');
+    };
+} elseif ($path === '/about') {
+    $action = function () {
+        return new HtmlResponse('I am a simple site');
+    };
+} elseif ($path === '/blog') {
+    $action = function () {
+        return new JsonResponse([
+            ['id' => 2, 'title' => 'The Second Post'],
+            ['id' => 1, 'title' => 'The First Post'],
+        ]);
+    };
+} elseif (preg_match('#^/blog/(?P<id>\d+)$#i', $path, $matches)) {
+    $request = $request->withAttribute('id', $matches['id']);
+    $action = function (ServerRequestInterface $request) {
+        $id = $request->getAttribute('id');
+        if ($id > 2) {
+            return new JsonResponse(['error' => 'Undefined page'], 404);
+        }
+        return new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
+    };
+}
+if ($action) {
+    $response = $action($request);
+} else {
+    $response = new HtmlResponse('Undefined page', 404);
+}
+### Postprocessing
+$response = $response->withHeader('X-Developer', 'ElisDN');
 
-$emitter = new ResponseSender();
-$emitter->send($response);
+### Sending
+$emitter = new SapiEmitter();
+$emitter->emit($response);
