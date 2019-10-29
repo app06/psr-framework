34dcea6d74214af5e9891860657e6c2e2aef354a
Sergey Nazarov <app06mt@gmail.com>
2019-10-29 17:55:17 +0300

    extracted router interface

--- src/Framework/Http/Router/Router.php
+++ src/Framework/Http/Router/Router.php
@@ -1,36 +1,24 @@
 <?php
+
 namespace Framework\Http\Router;
+
 use Framework\Http\Router\Exception\RequestNotMatchedException;
 use Framework\Http\Router\Exception\RouteNotFoundException;
 use Psr\Http\Message\ServerRequestInterface;
-class Router
+
+interface Router
 {
-    private $routes;
-
-    public function __construct(RouteCollection $routes)
-    {
-        $this->routes = $routes;
-    }
-
-    public function match(ServerRequestInterface $request): Result
-    {
-        foreach ($this->routes->getRoutes() as $route) {
-            if ($result = $route->match($request)) {
-                return $result;
-            }
-        }
-
-        throw new RequestNotMatchedException($request);
-    }
-
-    public function generate($name, array $params = []): string
-    {
-        foreach ($this->routes->getRoutes() as $route) {
-            if (null !== $url = $route->generate($name, $params)) {
-                return $url;
-            }
-        }
-
-        throw new RouteNotFoundException($name, $params);
-    }
+    /**
+     * @param ServerRequestInterface $request
+     * @throws RequestNotMatchedException
+     * @return Result
+     */
+    public function match(ServerRequestInterface $request): Result;
+    /**
+     * @param $name
+     * @param array $params
+     * @throws RouteNotFoundException
+     * @return string
+     */
+    public function generate($name, array $params): string;
 }
