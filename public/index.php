347eea1c5ccd5c0bd1b1e260b806a7d7992a6e78
Sergey Nazarov <app06mt@gmail.com>
2019-10-29 15:36:35 +0300

    add router

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+use Framework\Http\Router\Exception\RequestNotMatchedException;
+use Framework\Http\Router\RouteCollection;
+use Framework\Http\Router\Router;
 use Zend\Diactoros\ServerRequestFactory;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\JsonResponse;
@@ -10,45 +13,46 @@ chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
 ### Initialization
+$routes = new RouteCollection();
+
+$routes->get('home', '/', function (ServerRequestInterface $request) {
+    $name = $request->getQueryParams()['name'] ?? 'Guest';
+    return new HtmlResponse('Hello, ' . $name . '!');
+});
+$routes->get('about', '/about', function () {
+    return new HtmlResponse('I am a simple site');
+});
+$routes->get('blog', '/blog', function () {
+    return new JsonResponse([
+        ['id' => 2, 'title' => 'The Second Post'],
+        ['id' => 1, 'title' => 'The First Post'],
+    ]);
+});
+$routes->get('blog_show', '/blog/{id}', function (ServerRequestInterface $request) {
+    $id = $request->getAttribute('id');
+    if ($id > 2) {
+        return new HtmlResponse('Undefined page', 404);
+    }
+    return new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
+}, ['id' => '\d+']);
+$router = new Router($routes);
+
+### Running
 $request = ServerRequestFactory::fromGlobals();
-
-### Action
-$path = $request->getUri()->getPath();
-$action = null;
-
-if ($path === '/') {
-    $action = function (ServerRequestInterface $request) {
-        $name = $request->getQueryParams()['name'] ?? 'Guest';
-        return new HtmlResponse('Hello, ' . $name . '!');
-    };
-} elseif ($path === '/about') {
-    $action = function () {
-        return new HtmlResponse('I am a simple site');
-    };
-} elseif ($path === '/blog') {
-    $action = function () {
-        return new JsonResponse([
-            ['id' => 2, 'title' => 'The Second Post'],
-            ['id' => 1, 'title' => 'The First Post'],
-        ]);
-    };
-} elseif (preg_match('#^/blog/(?P<id>\d+)$#i', $path, $matches)) {
-    $request = $request->withAttribute('id', $matches['id']);
-    $action = function (ServerRequestInterface $request) {
-        $id = $request->getAttribute('id');
-        if ($id > 2) {
-            return new JsonResponse(['error' => 'Undefined page'], 404);
-        }
-        return new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
-    };
-}
-if ($action) {
+try {
+    $result = $router->match($request);
+    foreach ($result->getAttributes() as $attribute => $value) {
+        $request = $request->withAttribute($attribute, $value);
+    }
+    /** @var callable $action */
+    $action = $result->getHandler();
     $response = $action($request);
-} else {
+} catch (RequestNotMatchedException $e){
     $response = new HtmlResponse('Undefined page', 404);
 }
+
 ### Postprocessing
-$response = $response->withHeader('X-Developer', 'ElisDN');
+$response = $response->withHeader('X-Developer', 'Sergey');
 
 ### Sending
 $emitter = new SapiEmitter();
