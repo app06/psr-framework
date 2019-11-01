19fa225f539fdc4f686be93aaf7c407a9a91adda
Sergey Nazarov <app06mt@gmail.com>
2019-11-01 17:09:49 +0300

    add base auth decorator

--- src/App/Http/Action/CabinetAction.php
+++ src/App/Http/Action/CabinetAction.php
@@ -8,26 +8,9 @@ use Zend\Diactoros\Response\HtmlResponse;
 
 class CabinetAction
 {
-    private $users;
-
-    public function __construct(array $users)
-    {
-        $this->users = $users;
-    }
-
     public function __invoke(ServerRequestInterface $request)
     {
-        $username = $request->getServerParams()['PHP_AUTH_USER'] ?? null;
-        $password = $request->getServerParams()['PHP_AUTH_PW'] ?? null;
-
-        if (!empty($username) && !empty($password)) {
-            foreach ($this->users as $name => $pass) {
-                if ($username === $name && $password === $pass) {
-                    return new HtmlResponse('I am logged in as ' . $username);
-                }
-            }
-        }
-
-        return new EmptyResponse(401, ['WWW-Authenticate' => 'Basic realm=Restricted area']);
+        $username = $request->getAttribute('username');
+        return new HtmlResponse('I am logged in as ' . $username);
     }
 }
