baf7bfdc17a986d7539ed683b199b5a6bffdeab9
Sergey Nazarov <app06mt@gmail.com>
2019-11-01 16:04:21 +0300

    add users as array

--- public/index.php
+++ public/index.php
@@ -12,6 +12,10 @@ chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
 ### Initialization
+$params = [
+    'users' => ['admin' => 'password']
+];
+
 $aura = new Aura\Router\RouterContainer();
 $routes = $aura->getMap();
 
@@ -19,7 +23,7 @@ $routes->get('home', '/', Action\HelloAction::class);
 $routes->get('about', '/about', Action\AboutAction::class);
 $routes->get('blog', '/blog', Action\Blog\IndexAction::class);
 $routes->get('blog_show', '/blog/{id}', Action\Blog\ShowAction::class)->tokens(['id' => '\d+']);
-$routes->get('cabinet', '/cabinet', Action\CabinetAction::class);
+$routes->get('cabinet', '/cabinet', new Action\CabinetAction($params['users']));
 
 $router = new AuraRouterAdapter($aura);
 $resolver = new ActionResolver();
--- src/App/Http/Action/CabinetAction.php
+++ src/App/Http/Action/CabinetAction.php
@@ -8,13 +8,24 @@ use Zend\Diactoros\Response\HtmlResponse;
 
 class CabinetAction
 {
+    private $users;
+
+    public function __construct(array $users)
+    {
+        $this->users = $users;
+    }
+
     public function __invoke(ServerRequestInterface $request)
     {
         $username = $request->getServerParams()['PHP_AUTH_USER'] ?? null;
         $password = $request->getServerParams()['PHP_AUTH_PW'] ?? null;
 
-        if ($username === 'admin' && $password === 'password') {
-            return new HtmlResponse('Cabinet action');
+        if (!empty($username) && !empty($password)) {
+            foreach ($this->users as $name => $pass) {
+                if ($username === $name && $password === $pass) {
+                    return new HtmlResponse('I am logged in as ' . $username);
+                }
+            }
         }
 
         return new EmptyResponse(401, ['WWW-Authenticate' => 'Basic realm=Restricted area']);
