be5d040358ab261d3816b0d0fa27e6b1b804406e
Sergey Nazarov <app06mt@gmail.com>
2019-11-01 13:47:47 +0300

    add basic auth

--- src/App/Http/Action/CabinetAction.php
+++ src/App/Http/Action/CabinetAction.php
@@ -3,12 +3,20 @@
 namespace App\Http\Action;
 
 use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\EmptyResponse;
 use Zend\Diactoros\Response\HtmlResponse;
 
 class CabinetAction
 {
     public function __invoke(ServerRequestInterface $request)
     {
-        return new HtmlResponse('Cabinet action');
+        $username = $request->getServerParams()['PHP_AUTH_USER'] ?? null;
+        $password = $request->getServerParams()['PHP_AUTH_PW'] ?? null;
+
+        if ($username === 'admin' && $password === 'password') {
+            return new HtmlResponse('Cabinet action');
+        }
+
+        return new EmptyResponse(401, ['WWW-Authenticate' => 'Basic realm=Restricted area']);
     }
 }
