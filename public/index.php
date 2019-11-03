ab53760887862905ef9d494602a3f290c7550baa
Sergey Nazarov <app06mt@gmail.com>
2019-11-03 23:18:51 +0300

    add error handler middleware

--- public/index.php
+++ public/index.php
@@ -14,6 +14,7 @@ require 'vendor/autoload.php';
 
 ### Initialization
 $params = [
+    'debug' => true,
     'users' => ['admin' => 'password']
 ];
 
@@ -33,6 +34,7 @@ $router = new AuraRouterAdapter($aura);
 $resolver = new MiddlewareResolver();
 $app = new Application($resolver, new Middleware\NotFoundHandler());
 
+$app->pipe(new Middleware\ErrorHandlerMiddleware($params['debug']));
 $app->pipe(Middleware\ProfilerMiddleware::class);
 $app->pipe(Middleware\CredentialsMiddleware::class);
 
