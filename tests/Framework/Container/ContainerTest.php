bb1ec56b2682c1c54a5dd4c0403551d2ad063644
Sergey Nazarov <app06mt@gmail.com>
2019-11-13 21:57:01 +0300

    add container passing into callback

--- tests/Framework/Container/ContainerTest.php
+++ tests/Framework/Container/ContainerTest.php
@@ -46,6 +46,21 @@ class ContainerTest extends TestCase
         $container->get('email');
     }
 
+    public function testContainerPass()
+    {
+        $container = new Container();
+
+        $container->set('param', $value = 15);
+        $container->set($name = 'name', function (Container $container) {
+            $object = new \stdClass();
+            $object->param = $container->get('param');
+            return $object;
+        });
+
+        self::assertObjectHasAttribute('param', $object = $container->get($name));
+        self::assertEquals($value, $object->param);
+    }
+
     public function testSingleton()
     {
         $container = new Container();
