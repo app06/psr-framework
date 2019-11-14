9136391a9109b2551364eb1118116451695fc1f1
Sergey Nazarov <app06mt@gmail.com>
2019-11-14 22:56:30 +0300

    add container constructor definitions

--- config/dependencies.php
+++ config/dependencies.php
@@ -1,37 +1,35 @@
 <?php
 
 use App\Http\Middleware;
-use App\Http\Action;
 use Framework\Container\Container;
 use Framework\Http\Application;
-use Framework\Http\Middleware\DispatchMiddleware;
-use Framework\Http\Middleware\RouteMiddleware;
 use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Router\Router;
 use Zend\Diactoros\Response;
 
-/** @var Container $container */
-$container->set(Application::class, function (Container $container) {
-    return new Application(
-        $container->get(MiddlewareResolver::class),
-        $container->get(Router::class),
-        new Middleware\NotFoundHandler()
-    );
-});
+return [
+    Application::class => function (Container $container) {
+        return new Application(
+            $container->get(MiddlewareResolver::class),
+            $container->get(Router::class),
+            new Middleware\NotFoundHandler()
+        );
+    },
 
-$container->set(Middleware\BasicAuthMiddleware::class, function (Container $container) {
-    return new Middleware\BasicAuthMiddleware($container->get('config')['users']);
-});
+    Middleware\BasicAuthMiddleware::class => function (Container $container) {
+        return new Middleware\BasicAuthMiddleware($container->get('config')['users']);
+    },
 
-$container->set(Middleware\ErrorHandlerMiddleware::class, function (Container $container) {
-    return new Middleware\ErrorHandlerMiddleware($container->get('config')['debug']);
-});
+    Middleware\ErrorHandlerMiddleware::class => function (Container $container) {
+        return new Middleware\ErrorHandlerMiddleware($container->get('config')['debug']);
+    },
 
-$container->set(MiddlewareResolver::class, function (Container $container) {
-    return new MiddlewareResolver(new Response(), $container);
-});
+    MiddlewareResolver::class => function (Container $container) {
+        return new MiddlewareResolver(new Response(), $container);
+    },
 
-$container->set(Router::class, function () {
-    return new AuraRouterAdapter(new Aura\Router\RouterContainer());
-});
+    Router::class => function () {
+        return new AuraRouterAdapter(new Aura\Router\RouterContainer());
+    }
+];
