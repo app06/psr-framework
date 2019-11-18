3963f0293d17c8cfb387c702ebcc878a7ae4737f
Sergey Nazarov <app06mt@gmail.com>
2019-11-18 19:02:39 +0300

    add extention options

--- src/Framework/Template/Php/Extension.php
+++ src/Framework/Template/Php/Extension.php
@@ -4,6 +4,10 @@ namespace Framework\Template\Php;
 
 abstract class Extension
 {
+    /**
+     * @return SimpleFunction[]
+     */
+
     public function getFunctions(): array
     {
         return [];
