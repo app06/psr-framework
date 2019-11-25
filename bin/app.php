eacec58fd702f95c71deb8814e4c77179e5c9260
Sergey Nazarov <app06mt@gmail.com>
2019-11-26 01:14:22 +0300

    Ðinstall Symfony console

--- bin/app.php
+++ bin/app.php
@@ -2,28 +2,23 @@
 <?php
 
 use App\Console\Command\CacheClearCommand;
-use Framework\Console\Application;
 use Framework\Console\Input;
 use Framework\Console\Output;
+use Symfony\Component\Console\Application;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
 /**
  * @var \Psr\Container\ContainerInterface $container
- * @var \Framework\Console\Command[] $commands
  */
 $container = require 'config/container.php';
 
-$commands = [
-    $container->get(CacheClearCommand::class),
-];
-
-$cli = new Application();
+$cli = new Application('Application console');
 
 $commands = $container->get('config')['console']['commands'];
 foreach ($commands as $command) {
     $cli->add($container->get($command));
 }
 
-$cli->run(new Input($argv), new Output());
+$cli->run();
