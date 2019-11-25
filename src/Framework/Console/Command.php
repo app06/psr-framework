78209f47a08177264d7dc9854abe069f0ac15658
Sergey Nazarov <app06mt@gmail.com>
2019-11-25 22:26:00 +0300

    e

--- src/Framework/Console/Command.php
+++ src/Framework/Console/Command.php
@@ -9,6 +9,20 @@ abstract class Command
     private $name;
     private $description;
 
+    public function __construct(string $name = null)
+    {
+        if ($name !== null) {
+            $this->setName($name);
+        } else {
+            $this->setName(static::class);
+        }
+        $this->configure();
+    }
+
+    protected function configure(): void
+    {
+    }
+
     abstract public function execute(Input $input, Output $output): void;
 
     public function getName(): string
